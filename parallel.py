5c5109642657281124b2c507bcab42180b66356d
beatriz372 <74935128+beatriz372@users.noreply.github.com>
2021-01-11 19:41:36 -0600

    Add files via upload

---
+++ parallel.py
@@ -0,0 +1,13 @@
+import threading
+import time
+
+def hola_mundo(nombre):
+	print("Hola mundo" + nombre)
+	time.sleep(5)
+
+if __name__ == '__main__':
+	thread = threading.Thread(target = hola_mundo, args = ("Beatriz",))
+	thread.start()
+	thread.join()
+	print("Hola mundo desde el hilo principal")
+
